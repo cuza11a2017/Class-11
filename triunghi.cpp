9e4b63ec65be0246bed6c177b4b01b2d7ecb1271
cuza11a2017 <32458750+cuza11a2017@users.noreply.github.com>
2017-10-24 13:12:31 +0300

    Update triunghi.cpp

--- triunghi.cpp
+++ triunghi.cpp
@@ -1,28 +1,28 @@
 #include<iostream>
 #include<fstream>
 using namespace std;
-int t[50][50],s[50][50],n;
-ifstream f("triunghi.in");
+int triunghi[50][50],suma[50][50],n;
+ifstream f("triunghiriunghi.in");
 
 void citire()
 {
     f>>n;
     for(int i=1; i<=n; i++)
         for(int j=1; j<=i; j++)
-            f>>t[i][j];
+            f>>triunghi[i][j];
 }
 
 void prelucrare()
 {
     int i,j;
     for(j=1; j<=n; j++)
-        s[n][j]=t[n][j];
+        suma[n][j]=triunghi[n][j];
     for(i=n-1; i>=1; i--)
         for(j=1; j<=i; j++)
         {
-            s[i][j]=t[i][j]+s[i+1][j];
-            if(s[i+1][j]<s[i+1][j+1])
-                s[i][j]=t[i][j]+s[i+1][j+1];
+            suma[i][j]=triunghi[i][j]+suma[i+1][j];
+            if(suma[i+1][j]<suma[i+1][j+1])
+                suma[i][j]=triunghi[i][j]+suma[i+1][j+1];
         }
 }
 
@@ -30,8 +30,8 @@ void drum(int i,int j)
 {
     if(i<=n and j<=n)
     {
-        cout<<t[i][j]<<" ";
-        if(s[i+1][j]>s[i+1][j+1])
+        cout<<triunghi[i][j]<<" ";
+        if(suma[i+1][j]>suma[i+1][j+1])
             drum(i+1,j);
         else
             drum(i+1,j+1);
@@ -42,6 +42,6 @@ int main()
 {
     citire();
     prelucrare();
-    cout<<s[1][1]<<endl;
+    cout<<suma[1][1]<<endl;
     drum(1,1);
 }
